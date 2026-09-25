0e93d3944a2a82f560c0d86275de365d91c380df
VidalLeonardoDeLosSantosRincon <niggbibi@hotmail.com>
2026-09-24 22:08:31 -0400

    fix: error mapeo de propiedades (mapster)

--- TicketsAPI.Application/DependencyInjection.cs
+++ TicketsAPI.Application/DependencyInjection.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Mapster;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
 using TicketsAPI.Application.Interfaces.Services;
 using TicketsAPI.Application.Services;
 
@@ -9,6 +11,10 @@ public static class DependencyInjection
     public static IServiceCollection AddApplication(
     this IServiceCollection services)
     {
+        // Mapster
+        TypeAdapterConfig.GlobalSettings.Scan(Assembly.GetExecutingAssembly());
+        services.AddMapster();
+
         services.AddScoped<ITeamService, TeamService>();
 
         return services;
