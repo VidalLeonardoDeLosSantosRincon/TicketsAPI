4f95c7882223b0994228925b5055b89252f72b4c
VidalLeonardoDeLosSantosRincon <niggbibi@hotmail.com>
2026-09-26 02:36:31 -0400

    feat: agrega proyecto de pruebas unitarias con Nunit

---
+++ TicketsAPI.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace TicketsAPI.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
