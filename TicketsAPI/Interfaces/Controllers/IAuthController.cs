7d2222ad1a6432b313abd988daf4c1522c0eb6c7
VidalLeonardoDeLosSantosRincon <niggbibi@hotmail.com>
2026-09-26 01:11:25 -0400

    feat: documenta contrato de acción

--- TicketsAPI/Interfaces/Controllers/IAuthController.cs
+++ TicketsAPI/Interfaces/Controllers/IAuthController.cs
@@ -5,5 +5,13 @@ namespace TicketsAPI.Application.Interfaces.Controllers;
 
 public interface IAuthController
 {
+    /// <summary>
+    /// Genera un token de acceso en base a la información del usuario
+    /// </summary>
+    /// <param name="login">Este objeto de tipo <see cref="LoginResponseDto"/> contiene el correo y contraseña para generar el token de acceso</param>
+    /// <returns>
+    /// <para>Retorna un <see cref="Task{IActionResult}"/> de tipo <see cref="LoginResponseDto"/> con la información de la sessión,</para>
+    /// <para>o de tipo <see langword="null"/> si falló</para>
+    /// </returns>
     Task<IActionResult> Token(LoginDto login);
 }
